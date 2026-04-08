5f15ea119892a85afbd60c8f8e8adc5e0cd9f964
Delvane <sampaiodelvane@gmail.com>
2026-04-07 22:28:56 -0300

    Aula JS1

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 // alert("Hello World!!"); (CTRL+;) = comenta
-prompt("Qual é o seu nome?");
-confirm("Voce tem certeza disso?");
+// alert("Hello World!!");
+// prompt("Qual é o seu nome?");
+// confirm("Voce tem certeza disso?");
 
-console.log("teste!")
+// console.log("teste!")
+
+// let nome = "Delvane";
+// const pi = 3.1415;
+
+// console.log(1/0);
+// console.log("Texto/2");
+// console.log(typeof nome);
+
+let nome = prompt("Qual é o seu nome");
+alert(`Olá ${nome}!`)
