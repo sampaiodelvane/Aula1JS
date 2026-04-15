9f47985d009ae759b58ea969ede770a1646fc83c
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-14 21:53:22 -0300

    Aula14042026

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Aula 07/04/2026
+
 // alert("Hello World!!"); (CTRL+;) = comenta
 // alert("Hello World!!");
 // prompt("Qual é o seu nome?");
@@ -15,8 +17,31 @@
 // let nome = prompt("Qual é o seu nome");
 // alert(`Olá ${nome}!`)
 
-let nome = prompt("Qual é o seu nome")
-let age = prompt("Qual é a sua idade")
-let job = prompt("Qual sua profissão?")
+// let nome = prompt("Qual é o seu nome")
+// let age = prompt("Qual é a sua idade")
+// let job = prompt("Qual sua profissão?")
 
-alert(`Olá, ${nome}! Sua idade é ${age} anos e trabalha com ${job}`)
+// alert(`Olá, ${nome}! Sua idade é ${age} anos e trabalha com ${job}`)
+
+
+//AULA 14/04/2026
+
+// alert("!!!!!") //Ver se funcionou a conexão com o HTML
+
+//#ENTRADA - Convencer distância metros em centimetros
+// let distancia = prompt("Qual distância quer converter")
+
+// //PROCESSAMENTO
+// let resultado = distancia * 100;
+
+// //SAIDA
+// alert(`A Distancia ${distancia} metros e ${resultado} centimetros.`)
+
+//Exercício - Criar programa que calcule o IMC
+
+let peso = prompt("Qual seu peso?")
+let altura = prompt("Qual é a sua altura?")
+
+let resultado = peso / (altura * altura)
+
+alert(`Seu IMC é ${resultado}`)
